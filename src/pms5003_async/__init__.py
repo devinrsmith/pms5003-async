d1db634a53e0874743a813e2ea39215249c24bf8
Devin Smith <devinsmith@deephaven.io>
2022-06-19 11:26:22 -0700

    Bump version, more robust parsing

--- src/pms5003_async/__init__.py
+++ src/pms5003_async/__init__.py
@@ -1,16 +1,16 @@
 import aioserial
 import asyncio
+import contextlib
 import dataclasses
 import datetime
 import json
 import struct
 import sys
 import time
+import typing
 
-from contextlib import asynccontextmanager
 
-
-__version__ = '0.0.1.dev2'
+__version__ = '0.0.1.dev3'
 
 
 def _pretty_timestamp(timestamp: float) -> str:
@@ -74,37 +74,58 @@ class PMS5003Measurement:
         """Output the measurement as a JSON line"""
         timestamp_dict = {"timestamp": _pretty_timestamp(timestamp)} if timestamp else {}
         measurement_dict = dataclasses.asdict(self)
-        return json.dumps({**timestamp_dict, **measurement_dict})
+        return json.dumps({**timestamp_dict, **measurement_dict}, separators=(',', ':'))
 
 
 class DataError(RuntimeError):
     pass
 
 
-def parse(data: bytes) -> PMS5003Measurement:
-    if len(data) != 32:
-        raise DataError("Expected 32 bytes")
-    # todo first 4 bytes
-    unpacked = struct.unpack(">HHHHHHHHHHHHHH", data[4:])
-    actual_checksum = sum(data[:-2])
-    expected_checksum = unpacked[13]
-    if actual_checksum != expected_checksum:
-        raise DataError()
-    return PMS5003Measurement(*unpacked[:-1])
-
-
 class PMS5003Serial:
+
+    _FRAME_LENGTH = 28
+
+    #: char 1 == 0x42, char 2 == 0x47, frame length == 0x001c (28)
+    _START_SEQUENCE = b'\x42\x4d\x00\x1c'
+
     def __init__(self, aioserial: aioserial.AioSerial) -> None:
         self._aioserial = aioserial
 
 
-    async def read_one(self):
-        """Returns a tuple of the current timestamp and the PMS5003 measurement"""
-        data = await self._aioserial.read_async(32)
+    async def _read_start(self) -> bool:
+        for expected in PMS5003Serial._START_SEQUENCE:
+            c = ord(await self._aioserial.read_async(1))
+            if c != expected:
+                print(f"expected {expected}, actual {c}")
+                return False
+        return True
+
+
+    def _parse(frame: bytes) -> PMS5003Measurement:
+        unpacked = struct.unpack(">HHHHHHHHHHHHHH", frame)
+        actual_checksum = sum(PMS5003Serial._START_SEQUENCE + frame[:-2])
+        expected_checksum = unpacked[13]
+        if actual_checksum != expected_checksum:
+            return DataError()
+        return PMS5003Measurement(*unpacked[:-1])
+
+
+    async def try_read_one(self) -> typing.Optional[typing.Tuple[float, PMS5003Measurement]]:
+        if not await self._read_start():
+            return None
         timestamp = time.time()
-        return timestamp, parse(data)
+        frame = await self._aioserial.read_async(PMS5003Serial._FRAME_LENGTH)
+        return timestamp, PMS5003Serial._parse(frame)
 
 
+    async def read_one(self) -> typing.Tuple[float, PMS5003Measurement]:
+        # todo: limits on amount of bytes / time
+        while True:
+            item = await self.try_read_one()
+            if item:
+                return item
+
+    
     async def read(
         self,
         dedupe: bool = True,
@@ -127,12 +148,7 @@ class PMS5003Serial:
             yield ts, current
 
 
-    async def print_csv(self, file=sys.stdout):
-        async for _, current in self.read(dedupe=True, warmup=30.0):
-            print(current.csv(), file=file)
-
-
-@asynccontextmanager
+@contextlib.asynccontextmanager
 async def open_pms(port='/dev/ttyAMA0', baudrate=9600):
     _aioserial = aioserial.AioSerial(port=port, baudrate=baudrate)
     yield PMS5003Serial(_aioserial)
@@ -156,13 +172,19 @@ async def write_json(port='/dev/ttyAMA0', with_timestamp=True, dedupe=True, warm
 def write_csv_main():
     # TODO: argparse CLI
     print('Writing csv. May be short warmup delay...', file=sys.stderr)
-    asyncio.run(write_csv())
+    try:
+        asyncio.run(write_csv())
+    except KeyboardInterrupt:
+        sys.exit()
 
 
 def write_json_main():
     # TODO: argparse CLI
     print("Writing json. May be short warmup delay...", file=sys.stderr)
-    asyncio.run(write_json())
+    try:
+        asyncio.run(write_json())
+    except KeyboardInterrupt:
+        sys.exit()
 
 
 # TODO: single entrypoint, single main
